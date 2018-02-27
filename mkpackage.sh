acb179b23024621f7cb6f3cda243169c75befa35
wang yong <yongwc@yeah.net>
2018-02-27 10:45:40 +0800

    add support for queens, better the mkpackage tool

--- mkpackage.sh
+++ mkpackage.sh
@@ -2,54 +2,95 @@
 
 set -e
 
-# fixup the version str
-VERSION=${1:0:1}
-case $VERSION in
-  'j'|'J') 
-         VERSION='JUNO'
-         ;;
-  'k'|'K')
-         VERSION='KILO'
-         ;;
-  'l'|'L') 
-         VERSION='LIBERTY'
-         ;;
-  'm'|'M') 
-         VERSION='MITAKA'
-         ;;
-  'n'|'N')
-         VERSION='NEWTON'
-         ;;
-  'o'|'O') 
-         VERSION='OCATA'
-         ;;
-  'p'|'P') 
-         VERSION='PIKE'
-         ;;
-  *) 
-    echo "Version $1 not support"
-    echo "Usage $0 [j|k|l|m|n|o|p]"
-    exit 1
-    ;;
-  
-esac
+VERSION="1.0.0.0"
+COMMAND=$0
+
+# show help
+function show_usage() {
+    echo "Usage: $COMMAND [-h | -v | -t [j|k|l|m|o|p|q]]"
+    echo ""
+    echo "A tool to generate the package of inspur instorage cinder driver for specify Openstack version"
+    echo ""
+    echo "Options:"
+    echo "    -h    show this help"
+    echo "    -v    show the version"
+    echo "    -t V  specify the openstack version which"
+    echo "          we want generate package for, now we"
+    echo "          support version j k l m n o p q"
+}
+
+# deal with option
+while getopts "t:hv" arg
+do
+    case $arg in
+    t)
+        # openstack version as the arg, and fixup the version str
+        OS_VERSION=${OPTARG:0:1}
+        case $OS_VERSION in
+          'j'|'J') 
+                 OS_VERSION='JUNO'
+                 ;;
+          'k'|'K')
+                 OS_VERSION='KILO'
+                 ;;
+          'l'|'L') 
+                 OS_VERSION='LIBERTY'
+                 ;;
+          'm'|'M') 
+                 OS_VERSION='MITAKA'
+                 ;;
+          'n'|'N')
+                 OS_VERSION='NEWTON'
+                 ;;
+          'o'|'O') 
+                 OS_VERSION='OCATA'
+                 ;;
+          'p'|'P') 
+                 OS_VERSION='PIKE'
+                 ;;
+          'q'|'Q') 
+                 OS_VERSION='QUEENS'
+                 ;;
+          *) 
+            echo "OpenStack version $OPTARG not support"
+            show_usage
+            exit 1
+            ;;
+        esac
+        ;;
+    h)
+        show_usage
+        exit 0
+        ;;
+    v)
+        echo Version: $VERSION
+        exit 0
+        ;;
+    ?)
+        show_usage
+        exit 1
+        ;;
+    esac
+done
+
+echo "Generate Package for OpenStack version ${OS_VERSION}"
 
 # package name is G2_VERSION_cinder
-PACKAGE_NAME=G2_${VERSION}_cinder
+PACKAGE_NAME=G2_${OS_VERSION}_cinder
 
 # create the directory
 mkdir $PACKAGE_NAME
 
 # copy code and modify the RUN_VERSION placeholder
 cp -rf cinder/volume/drivers/inspur $PACKAGE_NAME/
-sed -i "s/@RUN_VERSION@/${VERSION}/" $PACKAGE_NAME/inspur/instorage/*
+sed -i "s/@RUN_VERSION@/${OS_VERSION}/" $PACKAGE_NAME/inspur/instorage/*
 
 # copy readme
 cp README.md $PACKAGE_NAME/
 
 # copy opt only when version early than mitaka
-if [[ $VERSION > 'LIBERTY' ]]
+if [[ $OS_VERSION > 'LIBERTY' ]]
 then
-  VERSION=$(echo $VERSION | tr -t [A-Z] [a-z])
-  cp cinder/opts-${VERSION}.py $PACKAGE_NAME/opts.py
+  OS_VERSION=$(echo $OS_VERSION | tr -t [A-Z] [a-z])
+  cp cinder/opts-${OS_VERSION}.py $PACKAGE_NAME/opts.py
 fi
